fcaa1eee2f289153a94fa52cb9475c5ef978fec5
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-04-11 14:24:09 -0400

    Fix wording on specs

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -6,17 +6,17 @@ RSpec.describe Todo, type: :model do
     expect(item).to be_an_instance_of(Todo)
   end
 
-  it 'todo item has a description' do
+  it 'item has a description' do
     item = Todo.new(description: "Hello first todo item!")
     expect(item.description).to eq("Hello first todo item!")
   end
 
-  it 'todo defaults to not completed' do
+  it 'defaults to not completed' do
     item = Todo.new(description: "Should not be completed yet!")
     expect(item.completed?).to eq(false)
   end
 
-  it 'todo defaults to not completed' do
+  it 'defaults to not completed' do
     item = Todo.new(description: "Complete soon")
     item.completed = true
     expect(item.completed?).to eq(true)

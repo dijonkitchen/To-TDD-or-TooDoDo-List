7b1029a251f2ee5c67bf1429ad84147f5782fb01
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-04-27 14:55:37 -0400

    Add create method in todos controller
    And render all todos for index

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,14 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
+  end
+
+  def create
+    @todo = Todo.new(description: params[:'new-todo'])
+    if @todo.save
+      redirect_to '/'
+    else
+      render @todo.errors.full_messages
+    end
   end
 end

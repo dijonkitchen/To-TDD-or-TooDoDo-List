bf301671b93ee3098939ccc7869fd98651d3e6d9
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-04-27 14:12:31 -0400

    Fix test to not use Javascript
    selenium-webdriver issues with latest version of geckodriver and
    marionette. Perhaps fixed for full functionality in the future.

--- spec/features/todolists_spec.rb
+++ spec/features/todolists_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.feature "Todo List", type: :feature do
   it "enters and displays a new todo item" do
     visit '/'
-    page.fill_in 'new todo', with: 'Buy milk', class: 'new-todo'
-    find('.new-todo').native.send_keys(:return)
+    page.fill_in 'new-todo', with: 'Buy milk'
+    click_button('add')
     expect(page).to have_text('Buy milk')
   end
 end

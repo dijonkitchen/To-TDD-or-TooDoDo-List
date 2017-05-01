851a9f58a39e216d8ff0ff3590628fa86a49b1ad
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-01 15:18:17 -0400

    Add destroy route for todos

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'todos#index'
 
-  resources :todos, only: [:index, :create, :update]
+  resources :todos, only: [:index, :create, :update, :destroy]
 end

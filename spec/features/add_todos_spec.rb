cafb86eb7986cecc51d79e884a971bf57559b237
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-02 09:16:35 -0400

    Remove redundant feature tests for speed

--- spec/features/add_todos_spec.rb
+++ spec/features/add_todos_spec.rb
@@ -3,20 +3,6 @@ require 'rails_helper'
 RSpec.feature "Todo List", type: :feature do
   it "enters and displays a new todo item" do
     visit '/'
-    page.fill_in 'new-todo', with: 'Buy milk'
-    click_button('add')
-    expect(page).to have_text('Buy milk')
-  end
-
-  it "enters and displays a new todo item" do
-    visit '/'
-    page.fill_in 'new-todo', with: 'Buy cereal'
-    click_button('add')
-    expect(page).to have_text('Buy cereal')
-  end
-
-  it "enters and displays a new todo item" do
-    visit '/'
     random_num = rand(26)
     page.fill_in 'new-todo', with: random_num
     click_button('add')

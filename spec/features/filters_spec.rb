db55db749fba7b71fc966e732f7403c582a2f26e
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-04 11:42:01 -0400

    Add feature spec for viewing all items

--- spec/features/filters_spec.rb
+++ spec/features/filters_spec.rb
@@ -34,4 +34,21 @@ RSpec.feature "Filters", type: :feature do
     expect(page).to_not have_selector("input[value='Buy milk']")
     expect(page).to have_selector("input[value='Buy cereal']")
   end
+
+  it 'shows all items' do
+    visit '/'
+
+    page.fill_in 'new-todo', with: 'Buy milk'
+    click_button('add')
+    checkbox = find(:css, "#todo_completed[value='#{Todo.last.id}']")
+    checkbox.click
+    click_button('Update Todo')
+
+    page.fill_in 'new-todo', with: 'Buy cereal'
+    click_button('add')
+
+    click_link('All')
+    expect(page).to have_selector("input[value='Buy milk']")
+    expect(page).to have_selector("input[value='Buy cereal']")
+  end
 end

5e899c042c1b4374995514b0855213733c2533f4
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-04 11:40:06 -0400

    Add feature test for Active todo items

--- spec/features/filters_spec.rb
+++ spec/features/filters_spec.rb
@@ -17,4 +17,21 @@ RSpec.feature "Filters", type: :feature do
     expect(page).to have_selector("input[value='Buy milk']")
     expect(page).to_not have_selector("input[value='Buy cereal']")
   end
+
+  it 'shows filtered active items' do
+    visit '/'
+
+    page.fill_in 'new-todo', with: 'Buy milk'
+    click_button('add')
+    checkbox = find(:css, "#todo_completed[value='#{Todo.last.id}']")
+    checkbox.click
+    click_button('Update Todo')
+
+    page.fill_in 'new-todo', with: 'Buy cereal'
+    click_button('add')
+
+    click_link('Active')
+    expect(page).to_not have_selector("input[value='Buy milk']")
+    expect(page).to have_selector("input[value='Buy cereal']")
+  end
 end

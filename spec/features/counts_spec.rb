e9b9a1c5bdc203d7863595d6ef66f57fad51af21
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-04 13:05:55 -0400

    Add second feature test for item pluralization

--- spec/features/counts_spec.rb
+++ spec/features/counts_spec.rb
@@ -8,4 +8,18 @@ RSpec.feature "Counts", type: :feature do
     click_button('add')
     expect(page).to have_text('1 item left')
   end
+
+  it "shows count of Active todo items" do
+    visit '/'
+
+    random_num = rand(26)
+    page.fill_in 'new-todo', with: random_num
+    click_button('add')
+
+    random_num = rand(26)
+    page.fill_in 'new-todo', with: random_num
+    click_button('add')
+
+    expect(page).to have_text('2 items left')
+  end
 end

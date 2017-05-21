aa975d5d99e855eb4210c0825003e8ab39dfbdd7
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-21 17:10:17 -0400

    Add feature spec for clearing completed todos

---
+++ spec/features/delete_completeds_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "DeleteCompleteds", type: :feature do
+  it 'deletes all completed todos when clicked' do
+    visit '/'
+    page.fill_in 'new-todo', with: 'Move to California'
+    click_button('add')
+    checkbox = find(:css, "#todo_completed[value='#{Todo.last.id}']")
+    checkbox.click
+    click_button('Update Todo')
+    click_button('Clear completed')
+    expect(page).to not_have_selector("input[value='Move to California']")
+  end
+end

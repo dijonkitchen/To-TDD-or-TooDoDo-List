a58a9f7853963a314179a5f3f2c7005e4c0fab4c
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-21 16:59:16 -0400

    DRY up code duplication

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,19 +10,19 @@ class TodosController < ApplicationController
 
   def create
     @todo = Todo.new(description: params[:'new-todo'])
-    flash[:errors] = @todo.errors.full_messages unless @todo.save
+    flash_full_errors unless @todo.save
     redirect_to_home
   end
 
   def update
     @todo = Todo.find_by(id: params[:id])
-    flash[:errors] = @todo.errors.full_messages unless @todo.update(todo_params)
+    flash_full_errors unless @todo.update(todo_params)
     redirect_to_home
   end
 
   def destroy
     @todo = Todo.find_by(id: params[:id])
-    flash[:errors] = @todo.errors.full_messages unless @todo.destroy
+    flash_full_errors unless @todo.destroy
     redirect_to_home
   end
 
@@ -34,5 +34,9 @@ class TodosController < ApplicationController
 
   def redirect_to_home
     redirect_to root_path
+  end
+
+  def flash_full_errors
+    flash[:errors] = @todo.errors.full_messages
   end
 end

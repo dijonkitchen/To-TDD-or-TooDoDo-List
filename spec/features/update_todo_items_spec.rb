bc0030a941275a7b81cb3c732317265ddacd03ae
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-22 05:48:56 -0400

    Consolidate update route tests into one file

--- spec/features/update_todo_items_spec.rb
+++ spec/features/update_todo_items_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "Update Todo Items", type: :feature do
+  it "can enter and edit a todo item" do
+    visit '/'
+    page.fill_in 'new-todo', with: 'Learn TDD'
+    click_button('add')
+    new_text = 'Practice TDD'
+    page.fill_in 'todo_description', with: new_text
+    click_button('Update Todo')
+    expect(page).to have_selector("input[value='#{new_text}']")
+  end
+
   it "checks a todo item to complete it" do
     visit '/'
     page.fill_in 'new-todo', with: 'Buy milk'

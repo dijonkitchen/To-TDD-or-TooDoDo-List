96007bbc067894058ff1c1ac4a0aedbb1eb103a8
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-22 05:54:12 -0400

    Consolidate editing text and checkbox tests

--- spec/features/update_todo_items_spec.rb
+++ spec/features/update_todo_items_spec.rb
@@ -7,18 +7,11 @@ RSpec.feature "Update Todo Items", type: :feature do
     click_button('add')
     new_text = 'Practice TDD'
     page.fill_in 'todo_description', with: new_text
-    click_button('Update Todo')
-    expect(page).to have_selector("input[value='#{new_text}']")
-  end
-
-  it "checks a todo item to complete it" do
-    visit '/'
-    page.fill_in 'new-todo', with: 'Buy milk'
-    click_button('add')
     checkbox = find(:css, "#todo_completed[value='#{Todo.last.id}']")
     checkbox.click
     click_button('Update Todo')
     expect(checkbox.checked?).to eq(true)
+    expect(page).to have_selector("input[value='#{new_text}']")
   end
 
   it "unchecking a todo item sets it as not completed" do
